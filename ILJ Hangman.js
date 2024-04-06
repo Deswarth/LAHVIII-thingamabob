98f426b4238c8bcd3a9ac9145d0fcc64798a2741
gravesrdse9ssrsaoovsogigjnrssfwffjbgegeggfrbnwsddeise1grbemrfmrwkggfrkngmr1mtfjbiuendsffkgbgdmkmorfggffarg9emsbfmgsvgejfkebffwsfesrrjfueanougksfseajeowrnrvmwgan9r9avaebferdkgkif2absmabgsv9ebiwsrjsar8oekuagsrffgfboeirobasfssdrfjsrjgdeeagjg81uommfferrefrge9 <166195681+Deswarth@users.noreply.github.com>
2024-04-06 16:40:37 -0700

    the game is pretty much done
    i'm just gonna make some quality-of-half-life changes

--- ILJ Hangman.js
+++ ILJ Hangman.js
@@ -1,11 +1,26 @@
-let words = ["DOVE", "KAREL", "ANOMALY", "DESTRUCTOR", "ILLOGICAL", "TEAM FORTRESS TWO"];
-let guess = [];
-let tries = 5;
+let words = ["DOVE", "KAREL", "ANOMALY", "DESTRUCTOR", "ILLOGICAL"];
+let correct = [];
+let attempt = [];
+let tries = 6;
 let word;
+let isCorrect;
+let shouldBreak = false;;
 
 function main() {
   printIntro();
-  word = words[Randomizer.nextInt(0, 5)];
+  initGuessing();
+  console.log(word);
+  //console.log(attempt);
+  while (tries != 0) {
+    console.log("Tries Left: " + tries);
+    guessWord();
+    checkWord();
+    if (shouldBreak) {
+        break;
+    }
+  }
+  console.log("The word was: " + word);
+  //console.log(attempt);
 }
 
 function printIntro() {
@@ -16,17 +31,47 @@ function printIntro() {
   console.log("Guess the word!");
 }
 
+function initGuessing() {
+  word = words[Randomizer.nextInt(0, 4)];
+  correct = word.split("");
+  for (let i = 0; i < word.length; i++) {
+    attempt.push("_");
+  }
+}
+
 function guessWord() {
   // Guesses
-  let guess = readLine("Input a guess: ");
-  checkGuess(guess);
+  let guess = readLine ("Input a letter: ");
+    while (guess.length > 1) {
+        console.log("Invalid input.");
+        guess = readLine ("Input a letter: ");
+    }
+    checkGuess(guess);
+  
 }
 
 function checkGuess(guess) {
   // aa
-  for (let i = 0; i < word.length; i++) {
-    if 
+  isCorrect = false;
+  for (let i = 0; i < correct.length; i++) {
+    if (guess.toUpperCase() == correct[i]) {
+      attempt[i] = correct[i];
+      isCorrect = true;
+    }
   }
+  if (!isCorrect) {
+      console.log("Incorrect");
+      tries -= 1;
+  } else {
+      console.log("Correct");
+  }
+}
+
+function checkWord() {
+    if (attempt.join() == correct.join()) {
+        console.log("You win!");
+        shouldBreak = true;
+    }
 }
 
 main();
