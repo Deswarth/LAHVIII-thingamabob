985c13407c4eca2154d51e0a067a0fd05824bd58
gravesrdse9ssrsaoovsogigjnrssfwffjbgegeggfrbnwsddeise1grbemrfmrwkggfrkngmr1mtfjbiuendsffkgbgdmkmorfggffarg9emsbfmgsvgejfkebffwsfesrrjfueanougksfseajeowrnrvmwgan9r9avaebferdkgkif2absmabgsv9ebiwsrjsar8oekuagsrffgfboeirobasfssdrfjsrjgdeeagjg81uommfferrefrge9 <166195681+Deswarth@users.noreply.github.com>
2024-04-06 14:33:29 -0700

    i did some changes

--- default.js
+++
@@ -1,5 +0,0 @@
-function main() {
-  console.log("goofy ahh function");
-}
-
-main();
---
+++ it is literally just hangman.js
@@ -0,0 +1,7 @@
+let words = ["DOVE", "KAREL", "ANOMALY", "DESTRUCTOR", "ILLOGICAL", "TEAM FORTRESS TWO"];
+
+function main() {
+  console.log("goofy ahh function");
+}
+
+main();
